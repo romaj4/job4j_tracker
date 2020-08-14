ded1056810c5e6ffe7b3c3371b3cf233f0553ad3
Roman Korolchuk <kororom@bk.ru>
2020-08-14 11:06:59 +0300

    Конфигурация Hibernate

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+create table HB_items (
+ 	id serial primary key,
+ 	name varchar(100),
+	description varchar(1000),
+	created bigint
+);
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,14 +1,26 @@
 package ru.job4j.tracker;
 
-public class Item {
+import javax.persistence.*;
+import java.util.Objects;
 
+@Entity
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column
     public String name;
 
+    @Column
     public String description;
 
+    @Column(name = "created")
     public long create;
+
+    public Item() {
+    }
 
     public Item(String name, String description) {
         this.name = name;
@@ -38,5 +50,37 @@ public class Item {
 
     public void setCreate(long create) {
         this.create = create;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) &&
+                Objects.equals(name, item.name) &&
+                Objects.equals(description, item.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
